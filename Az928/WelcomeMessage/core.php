7c95111cc4642528a4f06afaa90f0edb2bfdc863
Az928 <TheAz928@users.noreply.github.com>
2017-01-16 21:00:00 +0600

    Update core.php

--- Az928/WelcomeMessage/core.php
+++ Az928/WelcomeMessage/core.php
@@ -21,9 +21,9 @@ class core extends Base implements Listener{
 	  $this->getServer()->getLogger()->info("[WelcomeMessage] Loaded!");
 	  $this->getServer()->getPluginManager()->registerEvents($this,$this);
 	  @mkdir($this->getDataFolder());
-	  $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array("WelcomeMessage Config File by Az928#"));
-	  $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array("message" => "§aWelcome To the server!"));
-	  $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array("owner-name" => "none"));
+	  $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array("Tags#" => "Available tags are: {player}"));
+	  $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array("message" => "§aWelcome To the server, {player}!"));
+	  $this->cfg = new Config($this->getDataFolder()."config.yml", Config::YAML, array("owner-name" => "none", "owner-join-message" => "§aGuys, Server owner §e@Unknown §aJoined us!!!", "Note#" => "Prefix is used when useing /announce command", "prefix" => "§7[§aWelcome§cMessage§7]"));
 	  $this->saveResource("config.yml");
 	}
 	public function onDisable(){
@@ -31,26 +31,29 @@ class core extends Base implements Listener{
 	}
 	public function onJoin(PlayerJoinEvent $event){
 		$p = $event->getPlayer();
+		$j = $this->cfg->get("owner-join-message");
 		$msg = $this->cfg->get("message");
+		$msg = str_replace("{player}", $event->getPlayer()->getName(), $msg);
 		$name = $event->getPlayer()->getName();
 		$owner = $this->cfg->get("owner-name");
 		if($name === "$owner"){
-			$this->getServer()->broadcastMessage("§aThe Owner§c" .$name. "§eJoined the game!");
+			$this->getServer()->broadcastMessage($j);
 			$p->sendMessage("§aWelcome back," .TextFormat::GOLD.$name."§a!");
 		}else{
 			$p->sendMessage("$msg");
 	  	}
 		}
 		public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+			  $prefix = $this->cfg->get("prefix");
 		       switch($cmd->getName()){
 		           case "announce":
-		             $this->getServer()->broadcastMessage($args[0]);
+		             $this->getServer()->broadcastMessage($prefix . $args[0]);
 		           break;
 		           case "aboutwlc":
 		             $sender->sendMessage(TextFormat::GREEN."~~~WelcomeMesssge~~~");
-		             $sender->sendMessage("§eversion: §a1.1.0");
+		             $sender->sendMessage("§eversion: §a1.0.0");
 		             $sender->sendMessage("§a<====RecentChanges====>");
-		             $sender->sendMesssge("§e- Fixed Config.yml");
+		             $sender->sendMesssge("§e- Added commands + Config.yml");
 		             $sender->sendMesssge("§bPlugin by §6Az928, §aGitHub-TheAz928");
 		             break;
 		}
